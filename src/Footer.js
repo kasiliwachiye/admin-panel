d550b06bbbb66fe7f76c2f9d88c949e210188ae6
Emmanuel Wachiye <wachiye25@gmail.com>
2021-05-06 16:58:19 +0300

    First commit Message

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer class="main-footer">
+                    <strong>Copyright &copy; 2021 <a href="#">The Plug</a> . </strong>
+                    All rights reserved.
+                    <div class="float-right d-none d-sm-inline-block">
+                    <b>Version</b> 3.1.0
+                    </div>
+                </footer>
+            </div>
+        )
+    }
+}
